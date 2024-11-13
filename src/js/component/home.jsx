861547c12ece4b349ee69717dff045434453a251
antomanu95 <antoniomr.estevez@hotmail.com>
2024-11-13 22:13:55 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
+
+
+
+
 const Home = () => {
+
+	const [cosas, setCosas] = useState('');
+	const [deberes, setDeberes] = useState([]);
+console.log(deberes)
+	const handleSubmit = e => {
+		e.preventDefault();
+		setDeberes([...deberes,cosas]);
+		setCosas("")
+	}
+
+	const handleChange = (e) => {
+		setCosas(e.target.value);
+		console.log(e.target.value)
+	  };
+
+	const handleDelete = (index) => {
+		setDeberes(deberes.filter((_, i) => i!= index))
+	}
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="container">
+		<form onSubmit={handleSubmit}>
+		  <input
+			type="text"
+			onChange={handleChange}
+			value={cosas}
+			placeholder="Escribe una tarea..."
+		  />
+		</form>
+		<ul>
+		  {deberes.length > 0 ? (
+			deberes.map((cosas, i) => (
+			  <li key={i}>
+				{cosas}{" "}
+				<span className="delete-button" onClick={() => handleDelete(i)}>
+				  X
+				</span>
+			  </li>
+			))
+		  ) : (
+			<li> No hay tareas, añadir tareas</li>
+		  )}
+		</ul>
+		<p>Contador de tareas: {deberes.length}</p>
+	  </div>
 	);
-};
+  };
+  
 
 export default Home;
